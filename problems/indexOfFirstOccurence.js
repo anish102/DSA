3317a8729d6e55c230cb78905e77894b403effc5
anish102 <anishkarki695@gmail.com>
2024-04-02 09:03:39 +0545

    Leetcode problem

---
+++ problems/indexOfFirstOccurence.js
@@ -0,0 +1,31 @@
+var strStr = function (haystack, needle) {
+  let temp;
+  let flag;
+  for (let i = 0; i < haystack.length; i++) {
+    if (needle.length === 1) {
+      if (haystack[i] === needle) return i;
+    } else {
+      if (haystack[i] !== needle[0]) {
+        continue;
+      } else {
+        temp = i;
+        let k = i;
+        for (let j = 1; j < needle.length; j++) {
+          k++;
+          if (haystack[k] === needle[j]) {
+            flag = 1;
+          } else {
+            flag = 0;
+            break;
+          }
+        }
+        if (flag === 1) {
+          return temp;
+        }
+      }
+    }
+  }
+  return -1;
+};
+
+console.log(strStr("sadbutsad", "but"));
