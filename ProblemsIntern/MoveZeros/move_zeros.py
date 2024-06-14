2ae5f2a6c139bee3b414f916c5df3acd6d9e9e32
anish102 <anishkarki695@gmail.com>
2024-06-14 22:39:11 +0545

    Move zeros problem

--- ProblemsIntern/MoveZeros/move_zeros.py
+++ ProblemsIntern/MoveZeros/move_zeros.py
@@ -1,8 +1,7 @@
-nums = [0, 0, 1]
+nums = [0, 1, 0]
 left = 0
 for right in range(len(nums)):
     if nums[right] != 0:
         nums[left], nums[right] = nums[right], nums[left]
         left += 1
-
 print(nums)
