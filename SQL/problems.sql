5e15357c4265c3f418b0e781c9aca27bc1ec5777
anish102 <112316389+anish102@users.noreply.github.com>
2024-07-12 22:24:36 +0545

    Sql to find the record with odd id

--- SQL/problems.sql
+++ SQL/problems.sql
@@ -121,3 +121,10 @@ WHERE sales_id NOT IN (
 --Write a solution to swap all 'f' and 'm' values (i.e., change all 'f' values to 'm' and vice versa).
 update salary
 set sex = if(sex = 'm', 'f', 'm');
+
+--Write a solution to report the movies with an odd-numbered ID and a description that is not "boring". Return the result table ordered by rating in descending order.
+SELECT *
+FROM   cinema
+WHERE  description != "boring"
+       AND id % 2 != 0
+ORDER  BY rating DESC;
