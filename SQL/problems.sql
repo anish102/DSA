ab56acb11e65357dbb6e2126d393005bb441fefd
anish102 <anishkarki695@gmail.com>
2024-08-02 18:49:03 +0545

    SQL to find number of unique subjects

--- SQL/problems.sql
+++ SQL/problems.sql
@@ -192,3 +192,12 @@ FROM
 WHERE
     low_fats = "Y"
     AND recyclable = "Y";
+
+--Write a solution to calculate the number of unique subjects each teacher teaches in the university.
+SELECT
+    teacher_id,
+    COUNT(DISTINCT subject_id) AS cnt
+FROM
+    Teacher
+GROUP BY
+    teacher_id;
