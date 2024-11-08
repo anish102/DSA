09585adce581e6196cdda41d8832738d56f2bbd8
anish102 <anishkarki695@gmail.com>
2024-11-08 21:09:12 +0545

    Followers count

--- SQL/problems.sql
+++ SQL/problems.sql
@@ -220,3 +220,15 @@ WHERE
     Year(time_stamp) = 2020
 GROUP BY
     user_id;
+
+--Write a solution that will, for each user, return the number of followers.
+SELECT
+    user_id,
+    COUNT(follower_id) AS followers_count
+FROM
+    Followers
+GROUP BY
+    user_id
+ORDER BY
+    user_id ASC;
+    
