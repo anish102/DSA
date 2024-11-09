31d2e1359ded8d0fdaca204f3d1e28793e332a1f
anish102 <anishkarki695@gmail.com>
2024-11-09 20:44:56 +0545

    Remove log

--- ProblemsLeetcode/ReverseBits/reverse_bits.py
+++ ProblemsLeetcode/ReverseBits/reverse_bits.py
@@ -4,8 +4,6 @@ def reverseBits(n):
         bit = (n >> i) & 1
         print(bit)
         result = result | bit << (31 - i)
-        print(result)
-        print("\n")
     return result
 
 reverseBits(0b00000010100101000001111010011100)
